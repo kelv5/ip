66407fa0d6fbea656e11ba6fb652bd8301c5202a
kelv5 <ngkelvinkahkit@gmail.com>
2026-01-23 15:40:35 +0800

    Rename Duke.java to Fickle.java

--- src/main/java/Fickle.java
+++ src/main/java/Fickle.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Fickle {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
