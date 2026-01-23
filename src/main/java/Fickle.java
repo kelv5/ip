8ba6f3b8ef5d963269c97d4c004bf7c522bae61b
kelv5 <ngkelvinkahkit@gmail.com>
2026-01-23 18:50:42 +0800

    Add ASCII logo

--- src/main/java/Fickle.java
+++ src/main/java/Fickle.java
@@ -1,10 +1,57 @@
+/**
+ * Fickle chatbot main class.
+ */
 public class Fickle {
+    public static final String LINE = "___________________________________________________";
+
+    /**
+     * The entry point for the Fickle chatbot.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("___________________________________________________");
+        Fickle fickle = new Fickle();
+        fickle.run();
+    }
+
+    /**
+     * Runs the Fickle chatbot
+     * Displays the logo, greeting, and goodbye message.
+     */
+    public void run() {
+        printLogo();
+        greet();
+        sayGoodbye();
+    }
+
+    /**
+     * Displays the ASCII art logo.
+     */
+    private void printLogo() {
+        System.out.println(LINE);
+        String logo = "______ _      _    _\n"
+                + "|  ___(_)    | |  | |     \n"
+                + "| |_   _  ___| | _| | ___ \n"
+                + "|  _| | |/ __| |/ / |/ _ \\\n"
+                + "| |   | | |__|   <| |  __/\n"
+                + "\\_|   |_|\\___|_|\\_\\_|\\___|\n";
+        System.out.println(logo);
+    }
+
+    /**
+     * Prints Fickle's greeting message.
+     */
+    private void greet() {
         System.out.println(" Hello! I'm Fickle");
         System.out.println(" What shall we do today? ");
-        System.out.println("___________________________________________________");
-        System.out.println("Alright, that's enough for now. See you!");
-        System.out.println("___________________________________________________");
+        System.out.println(LINE);
+    }
+
+    /**
+     * Prints Fickle's goodbye message.
+     */
+    private void sayGoodbye() {
+        System.out.println(" Bye. See you soon!");
+        System.out.println(LINE);
     }
 }
