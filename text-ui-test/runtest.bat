ea536b04959c4af06c276a33a22b6113ca68348e
kelv5 <ngkelvinkahkit@gmail.com>
2026-01-29 17:38:13 +0800

    Update test scripts to compile Java files

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,8 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -Xlint:none -d ..\bin ..\src\main\java\fickle\*.java ..\src\main\java\fickle\tasks\*.java ..\src\main\java\fickle\ui\*.java ..\src\main\java\fickle\exceptions\*.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -15,7 +16,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Fickle < input.txt > ACTUAL.TXT
+java -classpath ..\bin fickle.Fickle < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,18 @@ then
     rm ACTUAL.TXT
 fi
 
+# compile all Java files recursively into the bin folder
+JAVA_FILES=$(find ../src/main/java -name "*.java")
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin $JAVA_FILES
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Fickle < input.txt > ACTUAL.TXT
+java -classpath ../bin fickle.Fickle < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
