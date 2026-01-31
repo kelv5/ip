48245d366c82381038a77b5a6af24ed100a6be60
kelv5 <ngkelvinkahkit@gmail.com>
2026-01-31 18:25:58 +0800

    Implement Command and subclasses handling command

---
+++ src/main/java/fickle/commands/DeleteCommand.java
@@ -0,0 +1,48 @@
+package fickle.commands;
+
+import fickle.exceptions.FickleException;
+import fickle.tasks.Task;
+import fickle.tasks.TaskList;
+import fickle.ui.Ui;
+
+/**
+ * Deletes a task in the task list.
+ */
+public class DeleteCommand extends Command {
+    private final int taskIndex;
+
+    /**
+     * Initialises a command that delete the task at the given index.
+     * 
+     * @param taskIndex The index of the task to be deleted, starting from 0
+     */
+    public DeleteCommand(int taskIndex) {
+        this.taskIndex = taskIndex;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) throws FickleException {
+        if (tasks.getSize() == 0) {
+            throw new FickleException("No tasks remaining to delete.", "A Little Happiness");
+        }
+        if (taskIndex < 0) {
+            throw new FickleException(
+                    "Invalid task index: less than 1.", "Too small, Insignificance");
+        }
+
+        if (taskIndex >= tasks.getSize()) {
+            throw new FickleException(
+                    "Invalid task index: exceeds the total number of tasks.", "Too Much");
+        }
+
+        Task task = tasks.deleteTask(taskIndex);
+        int totalTaskCount = tasks.getSize();
+        ui.printDeletedTask("  " + task.toString(), totalTaskCount);
+
+        // Special message when all tasks are deleted
+        if (totalTaskCount == 0) {
+            ui.printSingleLineWithoutLine("Wow! All tasks are deleted!");
+            ui.printEasterAlignedRight("A Little Happiness");
+        }
+    }
+}
---
+++ src/main/java/fickle/commands/UnmarkCommand.java
@@ -0,0 +1,42 @@
+package fickle.commands;
+
+import fickle.exceptions.FickleException;
+import fickle.tasks.Task;
+import fickle.tasks.TaskList;
+import fickle.ui.Ui;
+
+/**
+ * Unmarks a task in the task list.
+ */
+public class UnmarkCommand extends Command {
+    private final int taskIndex;
+
+    /**
+     * Initialises a command that unmarks the task at the given index.
+     * 
+     * @param taskIndex The index of the task to be unmarked, starting from 0
+     */
+    public UnmarkCommand(int taskIndex) {
+        this.taskIndex = taskIndex;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) throws FickleException {
+        if (tasks.getSize() == 0) {
+            throw new FickleException("No tasks remaining to unmark.", "A Little Happiness");
+        }
+        if (taskIndex < 0) {
+            throw new FickleException(
+                    "Invalid task index: less than 1.", "Too small, Insignificance");
+        }
+
+        if (taskIndex >= tasks.getSize()) {
+            throw new FickleException(
+                    "Invalid task index: exceeds the total number of tasks.", "Too Much");
+        }
+
+        Task task = tasks.getTask(taskIndex);
+        task.markAsNotDone();
+        ui.printUnmarkedTask(task.toString());
+    }
+}
