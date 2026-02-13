ed82c985372abee331cf3aa725ad50e08c861bb3
kelv5 <ngkelvinkahkit@gmail.com>
2026-02-14 02:51:10 +0800

    Refactor Fickle response and exception handling
    Fickle's message and exception handling are inconsistent and redundant.
    getResponse() makes unnecessary duplicate calls and run() violates SLAP.
    There is also unnecessary checks for the FickleException second line due
    to poor documentation of FickleException constructors.
    
    Let's:
    - Enforce SLAP in run() by introducing loadTasks(), addCorruptedWarnings(),
      and addGreetingMessages().
    - Remove redundant isExit check in getResponse() handled by byeCommand.
    - Remove duplicated return of ui.getOutput.
    - Simplify exception handling and update JavaDocs to clarify the usage.
    
    This refactor ensures readability and improve code quality.

--- src/main/java/fickle/exceptions/FickleException.java
+++ src/main/java/fickle/exceptions/FickleException.java
@@ -8,9 +8,10 @@ public class FickleException extends Exception {
     private String secondLine;
 
     /**
-     * Constructor for FickleException with a single message.
+     * Constructor for FickleException with a single main message.
+     * This constructor is only used for exceptions thrown during storage loading.
      *
-     * @param exceptionMessage The exception message.
+     * @param exceptionMessage The main exception message.
      */
     public FickleException(String exceptionMessage) {
         super(exceptionMessage);
@@ -18,24 +19,15 @@ public class FickleException extends Exception {
     }
 
     /**
-     * Constructor for FickleException with a main Exception message and an
-     * optional second line.
+     * Constructor for FickleException with a main message and a second line.
+     * This constructor is used for all other Fickle operations except storage loading.
      *
      * @param exceptionMessage The main exception message.
-     * @param secondLine An optional second line to display.
+     * @param secondLine An second line to display.
      */
     public FickleException(String exceptionMessage, String secondLine) {
         super(exceptionMessage);
         this.secondLine = secondLine;
-    }
-
-    /**
-     * Checks if this exception has a second line.
-     *
-     * @return True if there is a second line, false otherwise.
-     */
-    public boolean hasSecondLine() {
-        return !(secondLine == null);
     }
 
     /**
