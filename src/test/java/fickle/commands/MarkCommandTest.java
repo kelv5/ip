9b89a2cdfa15d1630b54a226eb308203b1a6031d
kelv5 <ngkelvinkahkit@gmail.com>
2026-02-14 22:32:59 +0800

    Update JUnit tests

--- src/test/java/fickle/commands/MarkCommandTest.java
+++ src/test/java/fickle/commands/MarkCommandTest.java
@@ -43,40 +43,4 @@ public class MarkCommandTest {
             assertEquals("There are no tasks left to mark.", e.getMessage());
         }
     }
-
-    @Test
-    public void markCommand_negativeIndex_exceptionThrown() {
-        TaskList tasks = new TaskList();
-        Ui ui = new Ui();
-        Storage storage = new Storage("data/test.txt");
-
-        Task task = new Todo("testing todo");
-        tasks.addTask(task);
-        MarkCommand cmd = new MarkCommand(-1);
-
-        try {
-            cmd.execute(tasks, ui, storage);
-            fail();
-        } catch (FickleException e) {
-            assertEquals("Task number starts from 1. Please enter a valid number.", e.getMessage());
-        }
-    }
-
-    @Test
-    public void markCommand_indexLargerThanSize_exceptionThrown() {
-        TaskList tasks = new TaskList();
-        Ui ui = new Ui();
-        Storage storage = new Storage("data/test.txt");
-
-        Task task = new Todo("testing todo");
-        tasks.addTask(task);
-        MarkCommand cmd = new MarkCommand(3);
-
-        try {
-            cmd.execute(tasks, ui, storage);
-            fail();
-        } catch (FickleException e) {
-            assertEquals("Please enter a task number within the available range.", e.getMessage());
-        }
-    }
 }
