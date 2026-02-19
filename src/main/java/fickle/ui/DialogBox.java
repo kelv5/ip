c5b37adab0365907e99efdda2dbd4d6baf1a7e38
kelv5 <ngkelvinkahkit@gmail.com>
2026-02-19 21:47:40 +0800

    Refactor constructor into helper for SLAP

--- src/main/java/fickle/ui/DialogBox.java
+++ src/main/java/fickle/ui/DialogBox.java
@@ -36,11 +36,19 @@ public class DialogBox extends HBox {
      *
      * @param mainText The main text content.
      * @param specialText The special text content (Optional: would be empty string for UserDialog).
+     * @param messageType The type of message for CSS styling.
      * @param img The current speaker's avatar image.
      */
     private DialogBox(String mainText, String specialText, String messageType, Image img) {
+        loadFxml();
+        setDialogText(mainText, specialText);
+        applyCssStyles(messageType);
+        setDisplayImage(img);
+    }
+
+    // Loads the FXML layout for the DialogBox and set current class as the controller.
+    private void loadFxml() {
         try {
-            // Load the FXML layout for the DialogBox and set current class as its controller
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
             fxmlLoader.setRoot(this);
@@ -48,7 +56,11 @@ public class DialogBox extends HBox {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    // Sets the main and special text for the dialog box.
+    // Hides the special label if the special text is empty.
+    private void setDialogText(String mainText, String specialText) {
         mainLabel.setText(mainText);
 
         // Displays a special text if it is present
@@ -56,22 +68,29 @@ public class DialogBox extends HBox {
             specialLabel.setText(specialText);
         } else {
             // Reused from https://stackoverflow.com/a/49053199 (fabian)
-            // Removes from layout and hides the label visually if there is no special text
+            // Removes from layout and hides the label visually.
             specialLabel.setManaged(false);
             specialLabel.setVisible(false);
         }
+    }
 
+    // Applies CSS styling to the dialog box and its labels.
+    private void applyCssStyles(String messageType) {
         // Applies CSS styling to labels
         mainLabel.getStyleClass().add("main-label");
         specialLabel.getStyleClass().add("special-label");
 
+        // Applies CSS styling to the dialog box.
         messageBox.getStyleClass().add("message-box");
 
         // Applies CSS styling to different type of Fickle messages.
-        if (messageType != null && !messageType.isEmpty()) {
+        if (messageType != null) {
             messageBox.getStyleClass().add(messageType);
         }
+    }
 
+    // Sets the profile image of the dialog box.
+    private void setDisplayImage(Image img) {
         displayPicture.setImage(img);
     }
 
